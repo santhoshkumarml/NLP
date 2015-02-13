90e5ecd5b317b3fc40fcd988efe8faf3b9378141
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-13 14:57:23 -0500

    Small changes

--- tryout/tryout/test.py
+++ tryout/tryout/test.py
@@ -3,5 +3,12 @@ Created on Feb 13, 2015
 
 @author: santhosh
 '''
+
+
 import nltk
-nltk.download()
+from nltk.corpus import brown
+from nltk import *
+nltk.data.path.append("/media/santhosh/Data/workspace/nltk_data")
+
+text = word_tokenize("And now for something completely different")
+print nltk.pos_tag(text)
