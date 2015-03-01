5ed1aaea6825a8a8d3c01e514eeb86a048f236a6
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-28 23:39:10 -0500

    Tests

--- tryout/tryout/test.py
+++ tryout/tryout/test.py
@@ -26,63 +26,64 @@ def split_word_and_pos(annotatedWord):
     wordAndPos.append(currentBuf)
     
     return wordAndPos
-            
-with open('../data1.txt') as f:
-    lines = f.readlines()
+
+
+def prepareData():
     train_data = []
     test_data = []
-    current_data = train_data
-    number_of_train_lines = 500
-    current_line = 0
-    for line in lines:
-        annotated_words = line.split()
-        words_tuples = []
-        for word_idx in range(1, len(annotated_words)):
-            annotatedWord = annotated_words[word_idx]
-            word,pos = split_word_and_pos(annotatedWord)
-            words_tuples.append((word,pos))
-    
-        current_data.append(words_tuples)
-        current_line += 1
-        if current_line == number_of_train_lines:
-            current_data = test_data
+    with open('../data1.txt') as f:
+        lines = f.readlines()
+        current_data = train_data
+        number_of_train_lines = 500
+        current_line = 0
+        for line in lines:
+            annotated_words = line.split()
+            words_tuples = []
+            for word_idx in range(1, len(annotated_words)):
+                annotatedWord = annotated_words[word_idx]
+                word, pos = split_word_and_pos(annotatedWord)
+                words_tuples.append((word, pos))
+            current_data.append(words_tuples)
+            current_line += 1
+            if current_line == number_of_train_lines:
+                current_data = test_data
+    return train_data, test_data
 
-# tagged_sents = nltk.corpus.treebank.tagged_sents()
-# train_data = tagged_sents[:500]
-# print train_data
+def predictAndEvaluate(posTagger, testData):
+    tagAccuracyDict = dict()
+    allTagsTotalDict = dict()
+    confusionMatrix = dict()
+
+    for sent_with_tags in test_data:
+        sent = [word for word, pos in sent_with_tags]
+        predicted_tag_pairs = posTagger.tag(sent)
+        for i in range(len(sent_with_tags)):
+            predicted_tag = predicted_tag_pairs[i][1]
+            real_tag = sent_with_tags[i][1]
+
+            if predicted_tag == real_tag:
+                if predicted_tag not in tagAccuracyDict:
+                    tagAccuracyDict[predicted_tag] = 0
+                tagAccuracyDict[predicted_tag] += 1
+
+            confusion_matrix_entry1 = (real_tag, predicted_tag)
+            confusion_matrix_entry2 = (predicted_tag, real_tag)
+
+            if confusion_matrix_entry1 not in confusionMatrix:
+                confusionMatrix[confusion_matrix_entry1] = 0
+                confusionMatrix[confusion_matrix_entry2] = 0
+            confusionMatrix[confusion_matrix_entry1] += 1
+
+            if real_tag not in allTagsTotalDict:
+                allTagsTotalDict[real_tag] = 0
+            allTagsTotalDict[real_tag] += 1
+
+    return tagAccuracyDict, allTagsTotalDict, confusionMatrix
+
+train_data, test_data = prepareData()
+
 bigramTagger = BigramTagger(train_data)
-tagAccuracyDict = dict()
-allTagsTotalDict = dict()
-confusionMatrix = dict()
-for sent_with_tags in test_data:
-    sent = [word for word, pos in sent_with_tags]
-    predicted_tag_pairs = bigramTagger.tag(sent)
-    for i in range(len(sent_with_tags)):
-        
-        predicted_tag = predicted_tag_pairs[i][1]
-        real_tag = sent_with_tags[i][1]
-        
-        if predicted_tag == real_tag:
-            if predicted_tag not in tagAccuracyDict:
-                tagAccuracyDict[predicted_tag] = 0
-            tagAccuracyDict[predicted_tag] += 1
-            
-        confusion_matrix_entry1 = (real_tag, predicted_tag)
-        confusion_matrix_entry2 = (predicted_tag, real_tag)
-        
-        if confusion_matrix_entry1 not in confusionMatrix:
-            confusionMatrix[confusion_matrix_entry1] = 0
-            confusionMatrix[confusion_matrix_entry2] = 0
-        confusionMatrix[confusion_matrix_entry1] += 1
-        
-        if real_tag not in allTagsTotalDict:
-            allTagsTotalDict[real_tag] = 0
-        allTagsTotalDict[real_tag] += 1
-        
-    
-print tagAccuracyDict
-print allTagsTotalDict
-print confusionMatrix
-        
-    
-#print bigramTagger.tag(treebank_sents[2007])
+
+tagAccuracyDict, allTagsTotalDict, confusionMatrix = predictAndEvaluate(bigramTagger, train_data)
+accuracyDict = {key: float(tagAccuracyDict[key])/float(allTagsTotalDict[key]) for key in tagAccuracyDict}
+print accuracyDict
