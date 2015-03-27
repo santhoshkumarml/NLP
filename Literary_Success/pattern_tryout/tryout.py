811cd35b891105ebfea46bb22f1cbeb0b9a447be
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-03-27 18:57:57 -0400

    changes

--- Literary_Success/pattern_tryout/tryout.py
+++ Literary_Success/pattern_tryout/tryout.py
@@ -8,3 +8,7 @@ for word, pos in tag('I feel *happy*!'):
     print word, pos
 s = parsetree('The cat sat on the mat.', relations=True, lemmata=True)
 print repr(s)
+from pattern.en import ngrams, sentiment
+print ngrams("I am eating pizza.", n=2) # bigrams
+
+print sentiment('Wonderfully awful! :-)').assessments
