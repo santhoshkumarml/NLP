1dbb3b653647c10a39407fa4a8e4fb707badc85a
Santhosh Kumar Manavasi Lakshminarayanan <santhoshkumarml@gmail.com>
2015-03-31 23:54:50 -0400

    Changes

--- Literary_Success/src/main/java/util/NovelMetaStanfordTagger.java
+++ Literary_Success/src/main/java/util/NovelMetaStanfordTagger.java
@@ -1,7 +1,18 @@
 package util;
 
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
 public class NovelMetaStanfordTagger {
-	public static void tagPos() {
-		
+	public static void main(String args[]) {
+		MaxentTagger tagger = new MaxentTagger("taggers/left3words-wsj-0-18.tagger");
+		//
+		// The sample string
+		String sample = "This is a sample text";
+
+		// The tagged string
+		String tagged = tagger.tagString(sample);
+
+		// Output the result
+		System.out.println(tagged);
 	}
 }
